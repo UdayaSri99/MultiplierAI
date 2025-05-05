5508734fd032c39f77916a77c27ba74b8221ac50
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2025-05-05 22:46:33 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -4,24 +4,22 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const todoRoutes = require('./routes/todoRoutes');
 
-// Load environment variables from .env
+
 dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // to parse JSON request bodies
 
-// MongoDB Connection
+app.use(cors());
+app.use(express.json()); 
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// Routes
-app.use('/api', todoRoutes);  // All Todo routes will be under '/api'
+app.use('/api', todoRoutes);  
 
-// Start the server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
