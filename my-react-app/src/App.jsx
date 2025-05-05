b1c5785e59cb99bc047e4263f4673d32fa5c7333
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2025-05-05 19:01:31 +0530

    commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AddTodo from './Components/AddTodo' ;
 import TodoList from './Components/TodoList' ;
+import './App.css'
 
 const App = () => {
   const [todos, setTodos] = useState([]);
